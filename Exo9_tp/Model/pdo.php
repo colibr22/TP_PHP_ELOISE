e7a9a4615382f63b7937fc9b0575bffef8fbe6ff
eloise <eloise.ansoult@student.junia.com>
2025-03-23 14:15:32 +0100

    sql

--- Exo9_tp/Model/pdo.php
+++ Exo9_tp/Model/pdo.php
@@ -2,7 +2,7 @@
 $servername = "localhost"; // Nom du serveur
 $username = "root"; // Nom d'utilisateur de la base de données
 $password = "root"; // Mot de passe de la base de données
-$dbname = "Junia"; // Nom de la base de données
+$dbname = "junia"; // Nom de la base de données
 
 try {
     $dbPDO = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
