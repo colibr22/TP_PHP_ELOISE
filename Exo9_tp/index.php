a4b8e9c21ba5d3c4755b86f0343cb35f0ee1601d
eloise <eloise.ansoult@student.junia.com>
2025-03-23 18:59:52 +0100

    liste prof

--- Exo9_tp/index.php
+++ Exo9_tp/index.php
@@ -1,6 +1,15 @@
 <?php
 require 'Model/pdo.php';
 
+$query = $pdo->query("SELECT nom, prenom FROM etudiants");
+$etudiants = $query->fetchAll(PDO::FETCH_ASSOC);
+
+echo "<h2>Liste des étudiants :</h2>";
+echo "<ul>";
+foreach ($etudiants as $etudiant) {
+    echo "<li>" . htmlspecialchars($etudiant['nom']) . " " . htmlspecialchars($etudiant['prenom']) . "</li>";
+}
+echo "</ul>";
 ?>
 
 <?php
@@ -11,6 +20,18 @@ echo "<h2>Liste des classes :</h2>";
 echo "<ul>";
 foreach ($classes as $classe) {
     echo "<li>" . htmlspecialchars($classe['nom_classe']) . "</li>";
+}
+echo "</ul>";
+?>
+
+<?php
+$query = $pdo->query("SELECT nom, prenom FROM professeurs");
+$professeurs = $query->fetchAll(PDO::FETCH_ASSOC);
+
+echo "<h2>Liste des professeurs :</h2>";
+echo "<ul>";
+foreach ($professeurs as $professeur) {
+    echo "<li>" . htmlspecialchars($professeur['nom']) . " " . htmlspecialchars($professeur['prenom']) . "</li>";
 }
 echo "</ul>";
 ?>
