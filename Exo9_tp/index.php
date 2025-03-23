649f92729678d7176c7d0277029523e2dd8898ef
eloise <eloise.ansoult@student.junia.com>
2025-03-23 14:14:12 +0100

    afficher liste

--- Exo9_tp/index.php
+++ Exo9_tp/index.php
@@ -1,3 +1,13 @@
 <?php
 require 'Model/pdo.php';
+
+$query = $pdo->query("SELECT nom, prenom FROM etudiants");
+$etudiants = $query->fetchAll(PDO::FETCH_ASSOC);
+
+echo "<h2>Liste des étudiants :</h2>";
+echo "<ul>";
+foreach ($etudiants as $etudiant) {
+    echo "<li>" . htmlspecialchars($etudiant['nom']) . " " . htmlspecialchars($etudiant['prenom']) . "</li>";
+}
+echo "</ul>";
 ?>
