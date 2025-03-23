18e70ce8657e5651bcee799199d23afc78c907ed
eloise <eloise.ansoult@student.junia.com>
2025-03-23 17:35:41 +0100

    liste classe

--- Exo9_tp/index.php
+++ Exo9_tp/index.php
@@ -11,3 +11,15 @@ foreach ($etudiants as $etudiant) {
 }
 echo "</ul>";
 ?>
+
+<?php
+$query = $pdo->query("SELECT nom_classe FROM classes");
+$classes = $query->fetchAll(PDO::FETCH_ASSOC);
+
+echo "<h2>Liste des classes :</h2>";
+echo "<ul>";
+foreach ($classes as $classe) {
+    echo "<li>" . htmlspecialchars($classe['nom_classe']) . "</li>";
+}
+echo "</ul>";
+?>
