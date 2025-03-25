307a2b6a431afb6c8b8986af242176d9181588ce
eloise <eloise.ansoult@student.junia.com>
2025-03-25 14:08:49 +0100

    2.1

--- Exo9_tp/index.php
+++ Exo9_tp/index.php
@@ -1,3 +1,19 @@
 <?php
 require 'Model/pdo.php';
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Liste des étudiants</title>
+</head>
+<body>
+    <h1>Liste des étudiants</h1>
+    <ul>
+        <?php foreach ($etudiants as $etudiant): ?>
+            <li><?= htmlspecialchars($etudiant['prenom'] . ' ' . $etudiant['nom']) ?></li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+</html>
