ec59ab4d1c3df510d78049b0216a538230edfce4
eloise <eloise.ansoult@student.junia.com>
2025-03-25 13:58:47 +0100

    reset

--- Exo9_tp/index.php
+++ Exo9_tp/index.php
@@ -35,3 +35,23 @@ foreach ($professeurs as $professeur) {
 }
 echo "</ul>";
 ?>
+
+<?php
+$query = $pdo->query("SELECT professeurs.nom AS prof_nom, professeurs.prenom AS prof_prenom, matieres.nom_matiere, classes.nom_classe
+    FROM professeurs
+    INNER JOIN matieres ON professeurs.id_matiere = matieres.id
+    INNER JOIN classes ON matieres.id_classe = classes.id
+");
+$professeurDetails = $query->fetchAll(PDO::FETCH_ASSOC);
+
+echo "<h2>Professeurs avec leur matière et classe :</h2>";
+echo "<ul>";
+foreach ($professeurDetails as $detail) {
+    echo "<li>" 
+        . htmlspecialchars($detail['prof_nom']) . " " . htmlspecialchars($detail['prof_prenom']) 
+        . " enseigne " . htmlspecialchars($detail['nom_matiere']) 
+        . " pour la classe " . htmlspecialchars($detail['nom_classe']) 
+        . "</li>";
+}
+echo "</ul>";
+?>
